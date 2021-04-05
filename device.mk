2d87b8adfeb2ca7554bd5cd9a5c092fddc321436
Jabiyeff <cebiyevanar@gmail.com>
2021-04-05 21:07:20 +0800

    vince: Import some perf blobs From LA.UM.8.6.2.r1-08400-89xx.0
    * Supposedly better app launching time
    
    Test: Booted and still testing, so far no errors
    
    Signed-off-by: Jabiyeff <cebiyevanar@gmail.com>
    Signed-off-by: GhostMaster69-dev <rathore6375@gmail.com>
    Signed-off-by: rayrzy <vincentrayananda@gmail.com>
    Signed-off-by: rk134 <rahul.kantrapally@gmail.com>

--- device.mk
+++ device.mk
@@ -78,6 +78,11 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.sip.voip.xml \
     frameworks/native/data/etc/android.software.vulkan.deqp.level-2020-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.vulkan.deqp.level.xml
 
+
+# Adapt Launch 
+PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/configs/lm/AdaptLaunchFeature.xml:$(TARGET_COPY_OUT_VENDOR)/etc/lm/AdaptLaunchFeature.xml
+
 # ANT
 PRODUCT_PACKAGES += \
     AntHalService \
