5351382e99d8886e7228d3d20a057989294ba78d
Rahul K <rahul-k@bigdi.cc>
2022-05-12 19:06:51 +0000

    vince: Update build fingerprint
    * bring back fingeprint overrides
    * 05/05/2022 patch (raven)
    
    Signed-off-by: Rahul K <rahul-k@bigdi.cc>

--- lineage_vince.mk
+++ lineage_vince.mk
@@ -47,3 +47,9 @@ TARGET_VENDOR := Xiaomi
 BOARD_VENDOR := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="vince-user 8.1.0 OPM1.171019.019 V11.0.2.0.OEGMIXM release-keys"
+
+# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
+BUILD_FINGERPRINT := "google/raven/raven:12/SP2A.220505.002/8353555:user/release-keys"
